e088f039ff904bc1965f2f1f5f853aed801af160
Harris Wong <harriswong@harriss-mbp.mynet>
2023-05-12 01:30:49 +0800

    add Todo task

--- app/src/main/java/com/example/comp4521_project/AddBillActivity.java
+++ app/src/main/java/com/example/comp4521_project/AddBillActivity.java
@@ -2,13 +2,22 @@ package com.example.comp4521_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class AddBillActivity extends AppCompatActivity {
 
+    EditText etTitle, etTotal;
     Button btnCreateBill;
+
+    ToggleButton tbEvenly, tbIndividually;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,42 @@ public class AddBillActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_bill);
 
         btnCreateBill = (Button) findViewById(R.id.btn_create_bill);
+        tbEvenly = (ToggleButton) findViewById(R.id.tb_evenly);
+        tbIndividually = (ToggleButton) findViewById(R.id.tb_individually);
+
+        tbEvenly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    buttonView.setBackgroundResource(R.drawable.active_toggle_button_background_with_border);
+                    tbIndividually.setChecked(false);
+                    tbIndividually.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.gray)));
+                }
+            }
+        });
+
+        tbIndividually.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    buttonView.setBackgroundResource(R.drawable.active_toggle_button_background_with_border);
+                    tbEvenly.setChecked(false);
+                    tbEvenly.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.gray)));
+                }
+            }
+        });
 
         btnCreateBill.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Todo: Add Bill info to Bill Table in database
+//                Todo: Check whether all view is filled + Add Bill info to Bill Table in database
+
+                String splitMode = tbEvenly.isChecked() ? "Evenly" : "Individually";
+
+                Toast.makeText(getApplicationContext(), splitMode, Toast.LENGTH_SHORT).show(); // display the current state of toggle button's
+
+                // Back to Home Activity
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/comp4521_project/ManageFriendsActivity.java
+++ app/src/main/java/com/example/comp4521_project/ManageFriendsActivity.java
@@ -6,24 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ManageFriendsActivity extends AppCompatActivity {
 
-    Button btnAddFriends;
+    EditText etFriendName;
+    Button btnAddFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_friends);
 
-        btnAddFriends = (Button) findViewById(R.id.btn_add_friends);
+        btnAddFriend = (Button) findViewById(R.id.btn_add_friend);
+        etFriendName = findViewById(R.id.et_friend_name);
 
-        btnAddFriends.setOnClickListener(new View.OnClickListener() {
+        btnAddFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Todo: Add Friend + Update Friend List + Friend Number + 1
+//              Todo: Check if the name exist in our database
+//                  if yes -> Add Friend to Friend List + Friend Number + 1
+//                  if no -> Toast.makeText(getApplicationContext(), "Non-exist User", Toast.LENGTH_SHORT).show();
+                String friendName = etFriendName.getText().toString();
             }
         });
-
     }
 }
