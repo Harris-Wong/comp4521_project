b1e5b610cd78fcd9f55ed55c8007bd2d1afb6b5f
keanehui <kawonghui@gmail.com>
2023-05-12 14:15:04 +0800

    implement settings

--- app/src/main/java/com/example/comp4521_project/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/comp4521_project/ui/settings/SettingsFragment.java
@@ -1,20 +1,86 @@
 package com.example.comp4521_project.ui.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.comp4521_project.R;
 import com.example.comp4521_project.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Spinner spinner = (Spinner) getView().findViewById(R.id.currencySpinner);
+        System.out.println(spinner == null ? "Is null" : "not null");
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.currencies_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String currentCurrency = sharedPref.getString(getString(R.string.text_currency), "");
+        switch (currentCurrency) {
+            case "HKD":
+                spinner.setSelection(0);
+                break;
+            case "USD":
+                spinner.setSelection(1);
+                break;
+            case "JPY":
+                spinner.setSelection(2);
+                break;
+            case "CNY":
+                spinner.setSelection(3);
+                break;
+            default:
+                spinner.setSelection(0);
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCurrency = (String) parent.getItemAtPosition(position);
+                SharedPreferences.Editor editor = getActivity().getPreferences(Context.MODE_PRIVATE).edit();
+                editor.putString(getString(R.string.text_currency), selectedCurrency);
+                editor.apply();
+//                Toast.makeText(getContext(), "Currency set to " + selectedCurrency, Toast.LENGTH_LONG).show();
+                Log.d("log", selectedCurrency);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button logoutButton = (Button) getView().findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("log", "Logout clicked");
+            }
+        });
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
